e76fed3b19a505ffe54af4fccc1f359a7d12cc4b
overallduka <overallduka@hotmail.com>
2015-05-30 18:42:31 -0300

    Added GoogleMaps SDK

--- ArcTouchRoutes/AppDelegate.swift
+++ ArcTouchRoutes/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyB08zc9qGv3qwCeL--kreEJUDcmA7dASL8")
+        
         return true
     }
 
